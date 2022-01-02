c4bac4a9b9bfea07f4491aef52a027968c028c9e
aarti <aartibansal567@gmail.com>
2022-01-02 21:52:28 +0530

    Add files via upload
    left triangle star pattern

---
+++ pattern.java
@@ -0,0 +1,19 @@
+class pattern
+{
+	public static void main(String[] args)
+	{
+		for(int i=0;i<=5 ; i++)
+		{
+			 for(int j = 0; j<5 ; j++)
+			 {
+				 if(i+j >= 5)
+					 System.out.print("*");
+				 else
+					 
+				  System.out.print(" ");
+			 }
+			  System.out.println();
+				 
+		}	 
+	}
+}
